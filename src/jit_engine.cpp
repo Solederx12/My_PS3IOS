11b733e097c5f0fc077a6f8f7a9cb73d00d6727f
Solederx12 <sarwarpubg999@gmail.com>
2026-08-30 01:34:47 +0300

    Update jit_engine.cpp

--- src/jit_engine.cpp
+++ src/jit_engine.cpp
@@ -1,24 +1,27 @@
 #include "../include/jit_engine.h"
 #include <sys/mman.h>
-#include <pthread.h>
 #include <stdexcept>
+#include <libkern/OSCacheControl.h>
 
 JITEngine::JITEngine(size_t size) : buffer_size(size) {
-    // دابینکردنی شوێنی execution له میمۆری بە هەمان یاسای iOS Dynamic Codesigning
+    // Allocation بە یاسای JIT Memory لە iOS
     code_buffer = mmap(NULL, buffer_size, PROT_READ | PROT_WRITE | PROT_EXEC,
-                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT, -1, 0);
+                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (code_buffer == MAP_FAILED) {
         throw std::runtime_error("JIT Allocation Failed");
     }
 }
 
 void JITEngine::write_code(const uint32_t* instructions, size_t count) {
-    pthread_jit_write_protect_np(0);
+    // گۆڕینی R/W ڕاستەوخۆ بە mprotect بۆ iOS
+    mprotect(code_buffer, buffer_size, PROT_READ | PROT_WRITE);
+    
     uint32_t* dest = static_cast<uint32_t*>(code_buffer);
     for (size_t i = 0; i < count; ++i) {
         dest[i] = instructions[i];
     }
-    pthread_jit_write_protect_np(1);
+    
+    mprotect(code_buffer, buffer_size, PROT_READ | PROT_EXEC);
     sys_icache_invalidate(code_buffer, count * sizeof(uint32_t));
 }
 
